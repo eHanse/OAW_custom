53e7d8c9ce2434899e74b11dcfac04cbc7341224
eHanseFrank <frank.schumacher@ehanse.de>
2017-12-13 12:34:56 +0800

    Revert "Renaming module etc."
    This reverts commit aec00f810f091eb90d2f464e07a65b2aafe7b75a.

--- products_sales/__openerp__.py
+++ products_sales/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "oa_products_sales_view",
+    'name': "products_sales_view",
 
     'summary': """
       Shows sales for each product. """,
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.8',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ["base",
