95f71bd1d67103d5b67b87d8f9940653e3ea43da
eHanseFrank <frank.schumacher@ehanse.de>
2018-03-30 11:58:48 +0800

    Description field set in all views but Return Transfers and Customer Invoices

--- oa_adj_description/model/stock_transfer_details_items_extended.py
+++ oa_adj_description/model/stock_transfer_details_items_extended.py
@@ -2,9 +2,9 @@
 from openerp import models, fields, api
 
 
-class StockTransferDetailsItems(models.Model):
+class StockTransferDetailsItems(models.TransientModel):
 
-    _inherit = 'stock_transfer_details_items'
+    _inherit = 'stock.transfer_details_items'
     _description = 'Adjust Description field in all Transfer views'
 
     new_description = fields.Char(
