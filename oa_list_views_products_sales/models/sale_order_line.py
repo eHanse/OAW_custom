8f8ccc319cb0b7002498b0e4665fae85527a9074
eHanseFrank <frank.schumacher@ehanse.de>
2018-05-10 09:24:23 +0800

    Make Products_Sales views consider foreign currencies

--- oa_list_views_products_sales/models/sale_order_line.py
+++ oa_list_views_products_sales/models/sale_order_line.py
@@ -6,8 +6,11 @@ import openerp.addons.decimal_precision as dp
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    #products_sales_qty = fields.Integer(string="Qty of sales of a product")
-
+    subtotal_hkd = fields.Float(
+        string="Subtotal in HKD",
+        digits=dp.get_precision('Product Price'),
+        readonly=True
+    )
 
     @api.multi
     def _get_amount(self, prod_tmpl_id):
@@ -16,16 +19,26 @@ class SaleOrderLine(models.Model):
             ('product_tmpl_id', '=', prod_tmpl_id),
             ('state','=', 'done'),
         ]
+        # SOLs with identical products
         sols = self.search(domain)
         sols_len = len(sols)
         if sols_len != 0:
-                for sol in sols:
-                    amount = amount + sol.price_subtotal
+            # We need the rate of the day of the confirmed Sales Order (in OA that date is relevant, not invoice date)
+            Rate = self.env['res.currency.rate']
+            for sol in sols:
+                date = sol.order_id.date_order
+                if date:
+                    rate = Rate.search([
+                        ('currency_id', '=', sol.order_id.currency_id.id),
+                        ('name', '<=', date),
+                    ], order='name desc', limit=1).rate or 1.0
+                subtotal_hkd = sol.price_subtotal / rate
+                amount = amount + sol.price_subtotal / rate
         else:
              amount =0
              sols_len = 1
 
-        res = [amount,sols_len]
+        res = [amount,sols_len,subtotal_hkd ]
         return res
 
     @api.multi
@@ -36,13 +49,15 @@ class SaleOrderLine(models.Model):
             res = self._get_amount(prod_tmpl.id)
             prod_tmpl.total = res[0]
             prod_tmpl.average = res[0]/res[1]
-        return
+
+        return res[2]
 
     # Will be executed whenever quotation is confirmed. The sol of the quotation exists already by then.
     # That is why sols of newly created quotation are considered when _update_prod_tmpl.... function is called.
     @api.multi
     def write(self, vals):
+        subtotal_hkd = self._update_prod_tmpl_amount_and_average()
+        vals['subtotal_hkd'] = subtotal_hkd
         res = super(SaleOrderLine, self).write(vals)
-        self._update_prod_tmpl_amount_and_average()
         return res
 
