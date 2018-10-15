eb9189f53b1084beb47f618970bed28f8ac36016
eHanseFrank <frank.schumacher@ehanse.de>
2018-10-15 10:50:53 +0800

    New Quotation Report added. Made Stock Pickings fields Source Document visible

---
+++ oa_stock_move_adjust/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Get Source Document",
+
+    'summary': """
+      Displays Stock Pickings Oirigin field in Stock Move. """,
+
+    'description': """
+
+    """,
+
+    'author': "OA Watches",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.7',
+
+    # any module necessary for this one to work correctly
+    'depends': ["base",
+                "stock",
+                "stock_view_adjust_oaw",
+                ],
+    # always loaded
+    'data': [
+#              "views/purchase_views.xml",
+        "views/stock_move_ext.xml",
+             ],
+    'installable': True,    
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
