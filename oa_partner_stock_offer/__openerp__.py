1439b866a34066098b45b47afcbd37a64d894741
tslai <tl@quartile.co>
2019-07-03 17:56:38 +0800

    [IMP] Add actions to update last modified, update field when partner stock is created

--- oa_partner_stock_offer/__openerp__.py
+++ oa_partner_stock_offer/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2017-2019 Quartile Limted
 # Copyright 2017 eHanse
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Partner Stock Offer',
-    'version': '8.0.0.9.0',
+    'version': '8.0.1.0.0',
     'author': 'eHanse,oa-trade,Quartile Limited,',
     'website': 'https://www.odoo-asia.com',
     'category': 'Product',
@@ -13,7 +13,6 @@
         'product_offer',
         'model_security_adjust_oaw',
         'supplier_stock'
-
     ],
     'description': """
         Creates a kanban view next to product_offer views based on supplier_stock model 'supplier_stock.py'\
@@ -22,6 +21,7 @@
 
     """,
     'data': [
+        'data/ir_actions.xml',
         'views/partner_stock_template_views.xml',
     ],
     'post_init_hook': '_update_partner_offer_fields',
--- oa_partner_stock_offer/models/__init__.py
+++ oa_partner_stock_offer/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Rooms For (Hong Kong) Limted T/A OSCG
+# Copyright 2017-2019 Quartile Limted
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-#
 
+from . import product_product
 from . import supplier_stock
 from . import supplier_location_ext
