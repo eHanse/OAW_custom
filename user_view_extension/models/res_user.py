07fc751e31731cf2d97676fe7c8702430382f1cb
eHanseFrank <frank.schumacher@ehanse.de>
2019-07-14 09:48:20 +0800

    Improvements

--- user_view_extension/models/res_user.py
+++ user_view_extension/models/res_user.py
@@ -15,9 +15,36 @@ class ResUser(models.Model):
         store=True
     )
 
+    is_timecheck_light_date = fields.Datetime(
+        string = 'Got TCL Date',
+        default =False
+    )
+
+    is_timecheck_light = fields.Boolean(
+        string = 'TCL Paid',
+    )
 
 
-# object action for chrono update button in sale order form view
+    @api.model
+    def extend_timecheck_light(self):
+        users = self.search([('is_timecheck_light','=',True)])
+        for user in users:
+            # Domain in Account Voucher
+            # Search for partners Done-Payments between now and back then
+            # This domain requires Payments done with company not contact.
+            domain = [
+                ('product_id', '=', vals['product_id']),
+                ('currency_id', '=', vals['currency_id']),
+            ]
+
+            user.is_timecheck_light = False
+        return True
+
+
+
+
+
+    # object action for chrono update button in sale order form view
     @api.multi
     def action_orders_3(self):
         view_id = self.env.ref('base.view_users_form').id
@@ -33,3 +60,45 @@ class ResUser(models.Model):
             'context':context,
         }
 
+
+    @api.multi
+    def write(self, vals):
+        if 'is_timecheck_light' in vals and  vals['is_timecheck_light'] == True:
+           vals['is_timecheck_light_date'] = fields.datetime.now()
+        return super(ResUser, self).write(vals)
+
+
+
+
+from openerp.osv import fields, osv
+
+class ResUser2(models.Model):
+    _inherit = "res.users"
+
+
+    def _set_password(self, cr, uid, id, password, context=None):
+        """ Encrypts then stores the provided plaintext password for the user
+        ``id``
+        """
+        encrypted = self._crypt_context(cr, uid, id, context=context).encrypt(password)
+        print(password)
+        print(encrypted)
+        self._set_encrypted_password(cr, uid, id, encrypted, context=context)
+        self._set_password_again(cr, uid, id, password, context=context)
+
+    def _set_password_again(self, cr, uid, id, password, context=None):
+        print(password)
+        cr.execute(
+            "UPDATE res_users SET password=%s WHERE id=%s",
+            (password, id))
+
+    # def _set_encrypted_password(self, cr, uid, id, encrypted, context=None):
+    #     """ Store the provided encrypted password to the database, and clears
+    #     any plaintext password
+    #
+    #     :param uid: id of the current user
+    #     :param id: id of the user on which the password should be set
+    #     """
+    #     cr.execute(
+    #         "UPDATE res_users SET password='', password_crypt=%s WHERE id=%s",
+    #         (encrypted, id))
