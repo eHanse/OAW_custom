dd334d3fb41f374cb60f670e72721b1605cfd979
eHanseFrank <frank.schumacher@ehanse.de>
2019-09-12 17:51:14 +0800

    Corrected hook regarding default_cod

--- oa_adj_description/hooks.py
+++ oa_adj_description/hooks.py
@@ -7,84 +7,65 @@ def _update_description_field(cr, registry):
            UPDATE
            purchase_order_line pol
            SET
-           new_description= subquery.name
+           default_code = subquery.default_code
            FROM (
                SELECT
-               pt.name AS name,
-               pol.product_id AS prod_id
+               pp.default_code AS default_code,
+               pp.id AS prod_id
                FROM
-               product_template pt
-               JOIN product_product pp ON pt.id = pp.product_tmpl_id
-               JOIN purchase_order_line pol ON pol.product_id= pp.id
+               product_product pp
                ) AS subquery
-           WHERE pol.product_id = subquery.prod_id
+           WHERE subquery.prod_id = pol.product_id
        ''')
     # updates new_description field in stock move
     cr.execute('''
     UPDATE
         stock_move sm
     SET
-        new_description= subquery.name
+        default_code= subquery.default_code
         FROM (
-          SELECT
-            pt.name AS name,
-            sm.product_id AS prod_id
-          FROM
-            product_template pt
-            JOIN product_product pp ON pt.id = pp.product_tmpl_id
-            JOIN stock_move sm ON sm.product_id= pp.id
+          SELECT pp.id AS product_id, pp.default_code AS default_code
+          FROM product_product pp
           ) AS subquery
-    WHERE sm.product_id = subquery.prod_id
+    WHERE sm.product_id = subquery.product_id
     ''')
     # updates new_description field in account_invoice_line
     cr.execute('''
-   UPDATE
-       account_invoice_line ail
-   SET
-       new_description= subquery.name
-       FROM (
-         SELECT
-           pt.name AS name,
-           ail.product_id AS prod_id
-         FROM
-           product_template pt
-           JOIN product_product pp ON pt.id = pp.product_tmpl_id
-           JOIN account_invoice_line ail ON ail.product_id= pp.id
-         ) AS subquery
-   WHERE ail.product_id = subquery.prod_id
+
    ''')
 
     # updated new_description field in quants tree view
+    cr.execute('''UPDATE
+       account_invoice_line ail
+       SET
+       default_code= subquery.default_code
+          FROM (
+          SELECT pp.id AS product_id, pp.default_code AS default_code
+          FROM product_product pp
+         ) AS subquery
+       WHERE ail.product_id = subquery.product_id
+
+       ''')
     cr.execute('''
          UPDATE
              stock_quant sq
          SET
-             new_description= subquery.name
-             FROM (
-               SELECT
-                 pt.name AS name,
-                 sq.product_id AS prod_id
-               FROM
-                 product_template pt
-                 JOIN product_product pp ON pt.id = pp.product_tmpl_id
-                 JOIN stock_quant sq ON sq.product_id = pp.id
-               ) AS subquery
-         WHERE sq.product_id = subquery.prod_id
+         default_code= subquery.default_code
+              FROM (
+              SELECT pp.id AS product_id, pp.default_code AS default_code
+              FROM product_product pp
+         ) AS subquery
+         WHERE sq.product_id = subquery.product_id
      ''')
     # new_description field in transfer views
     cr.execute('''
         UPDATE
             stock_transfer_details_items items
         SET
-            new_description= subquery.name
+            default_code= subquery.default_code
             FROM (
-              SELECT
-                pt.name AS name,
-                items.product_id AS prod_id
-              FROM
-                product_template pt
-                JOIN product_product pp ON pt.id = pp.product_tmpl_id
-                JOIN stock_transfer_details_items items ON items.product_id = pp.id
-              ) AS subquery
-        WHERE items.product_id = subquery.prod_id
+              SELECT pp.id AS product_id, pp.default_code AS default_code
+              FROM product_product pp
+         ) AS subquery
+        WHERE items.product_id = subquery.product_id
     ''')
